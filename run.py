737fe1b999165ff18737865da3b93f9ddbe7e019
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2025-11-13 00:46:42 +0100

    Adding RPC v5 by hlnajz

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # fallback for local testing
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
